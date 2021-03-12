0df80d3e0384c4eec0d8d88a056607cda9b0d2c2
SarahOorts <r0657122@student.thomasmore.be>
2021-03-12 19:32:13 +0100

    Added Weather API to getWeather + extracted temperature

--- lab 4/lib/index.js
+++ lab 4/lib/index.js
@@ -25,6 +25,30 @@ var Recipe = /*#__PURE__*/function () {
         _this.getWeather(lat, _long);
       });
     }
+  }, {
+    key: "getWeather",
+    value: function getWeather(lat, _long2) {
+      var url = "http://www.7timer.info/bin/api.pl?lon=".concat(_long2, "&lat=").concat(lat, "&product=civillight&output=json");
+      console.log(url);
+      console.log("hello");
+      fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (json) {
+        console.log(":D");
+        console.log(json);
+        var js = json.dataseries[0];
+        console.log(js);
+        var celsius = js.temp2m;
+        console.log(celsius);
+        var temp = celsius.max;
+        console.log(temp); // let temperature = json.dataseries.splice(0, 1);
+        // console.log(temperature);
+        // let temp = JSON.stringify(temperature);
+        // console.log(temp);
+        // let t = temp.splice(2,1);
+        // console.log(t);
+      });
+    }
   }]);
 
   return Recipe;
--- lab 4/src/index.js
+++ lab 4/src/index.js
@@ -10,6 +10,26 @@ class Recipe {
             this.getWeather(lat, long);
         });
     }
+
+    getWeather(lat, long) {
+        let url = `http://www.7timer.info/bin/api.pl?lon=${long}&lat=${lat}&product=civillight&output=json`;
+        console.log(url);
+        console.log("hello");
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(":D");
+            console.log(json);
+            let js = json.dataseries[0];
+            console.log(js);
+            let celsius = js.temp2m;
+            console.log(celsius);
+            let temp = celsius.max;
+            console.log(temp);
+        });
+    }
     }
     
     let recipe = new Recipe();
