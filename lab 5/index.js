24301be6cca2efb04947643786610c5f935a0676
SarahOorts <r0657122@student.thomasmore.be>
2021-03-17 20:16:36 +0100

    added controllers to routes + changed gitignore file

--- lab 5/index.js
+++ lab 5/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const routerMessages = require("./routes/index.js");
-
-app.set('view engine', "pug");
+const routerMessages = require("./routes/index");
 
 app.get('/', (req, res) => {
+    app.set('view engine', "pug");
 //   res.send('Hello World!')
   //homepage pug
   res.render('index', { title: 'Lab 5', message: 'Hello there!'});
--- lab 5/routes/index.js
+++ lab 5/routes/index.js
@@ -1,65 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const messageController = require("../controllers/index");
 
-router.get("/", (req, res) => {
-  //mongoDB return messages
-  // zonder mongoDB res { message : "GETTING messages"}
-  res.json({
-      message : "GETTING messages",
-  });    
-});
+router.get("/", messageController.getM);
 
-router.get("/:id", (req, res) => {
-  // res.send("Hello World!")
-  let id = req.params.id;
-  //mongoDB message met id
-  // zonder mongoDB res { message : `GETTING messages with ID ${id}`}
-  res.json({
-      message : `GETTING messages with ID ${id}`,
-  }); 
-});
+router.get("/:id", messageController.getMId);
 
-router.post("/:user", (req, res) => {
-  // res.send("Hello World!")
-  //mongoDB JSON-object ontv, local storage
-  // body { message: { “user”: “Pikachu”, “text”: “nodejs isn’t hard, or is it?” } }
-  //zonder mongoDB res {“message”: “POSTING a new message for user Pikachu”}
-  //naam dynamisch invullen + teruggeven
-  let user = req.params.user;
-  res.json({
-      message : `POSTING a new message for user ${user}`,
-  });
-});
+router.post("/:user", messageController.postUser);
 
-router.put("/:id", (req, res) => {
-  //mongoDB JSON local storage update
-  //zonder mongoDB res  {“message”: “UPDATING a message with id id”}
-  let id = req.params.id;
+router.put("/:id", messageController.putId);
 
-  res.json({
-      message : `UPDATING a message with ID ${id}`,
-  }); 
-});
+router.delete("/:id", messageController.deleteId);
 
-router.delete("/:id", (req, res) => {
-  //mongoDB remove message met id, res {“status”: “success”, “message”: “The message was removed”}
-  //zonder mongoDB res  {“message”: “DELETING a message with id id”}
-  let id = req.params.id;
-
-  res.json({
-      message : `DELETING a message with ID ${id}`,
-  }); 
-});
-
-router.get("/?user=username", (req, res) => { 
-  //mongoDB return messages user
-  //req.params.username!!
-  //zonder mongoDB res  {“message”: “GETTING message for username username”}
-  let username = req.query.username;
-
-  res.json({
-      message : `GETTING message for username ${username}`,
-  }); 
-});
+router.get("/?user=username", messageController.getUsername);
 
 module.exports = router;
