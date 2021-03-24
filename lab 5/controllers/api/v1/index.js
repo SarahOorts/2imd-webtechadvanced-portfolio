e45f3ab7cb7ea0cd26fac9666dc61c89a57c1ac4
SarahOorts <r0657122@student.thomasmore.be>
2021-03-24 15:44:48 +0100

    (fix) - add content lab 5 from lab5heroku repository (https://github.com/SarahOorts/lab5heroku.git) to lab 5 imd-webtechadvanced-portfolio repository

--- lab 5/controllers/api/v1/index.js
+++ lab 5/controllers/api/v1/index.js
@@ -1,10 +1,21 @@
 function getM(req, res){
     //mongoDB return messages
    // zonder mongoDB res { message : "GETTING messages"}
+   if(req.query.user != null){
+    let username = req.query.user;
+    console.log(username);
+    res.json({
+        status : "succes",
+        message : `GETTING message for username ${username}`,
+    });
+   } 
+   else{
+    
     res.json({
         status : "succes",
         message : "GETTING messages",
     });
+   }    
 }
 
 function getMId(req, res){
@@ -24,10 +35,10 @@ function postUser(req, res){
     // body { message: { “user”: “Pikachu”, “text”: “nodejs isn’t hard, or is it?” } }
     //zonder mongoDB res {“message”: “POSTING a new message for user Pikachu”}
     //naam dynamisch invullen + teruggeven
-    let user = req.params.user;
+    // let user = req.params.user;
     res.json({
         status : "succes",
-        message : `POSTING a new message for user ${user}`,
+        message : `POSTING a new message for user Pikachu`, //${user}
     });
 }
 
@@ -53,22 +64,24 @@ function deleteId(req, res){
     }); 
   }
 
-  function getUsername(req, res){ 
-    //mongoDB return messages user
-    //req.params.username!!
-    //zonder mongoDB res  {“message”: “GETTING message for username username”}
-    let username = req.params.username;
+//   function getUsername(req, res){ 
+//     //mongoDB return messages user
+//     //req.params.username!!
+//     //zonder mongoDB res  {“message”: “GETTING message for username username”}
+//     let username = req.param("user");
+//     // let user = JSON.stringify(username); //https://www.codegrepper.com/code-examples/javascript/node+http+get+query+params
+//     // console.log(user); //https://expressjs.com/en/guide/routing.html
     
-    res.json({
-        status : "succes",
-        message : `GETTING message for username ${username}`,
-    }); 
+//     res.json({
+//         status : "succes",
+//         message : `GETTING message for username ${username}`,
+//     }); 
     
-  }
+//   }
 
 module.exports.getM = getM;
 module.exports.getMId = getMId;
 module.exports.postUser = postUser;
 module.exports.putId = putId;
 module.exports.deleteId = deleteId;
-module.exports.getUsername = getUsername;
+// module.exports.getUsername = getUsername;
--- lab 5/routes/api/v1/index.js
+++ lab 5/routes/api/v1/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const messageController = require("../controllers/index");
+const messageController = require("../../../controllers/api/v1/index");
 
 router.get("/", messageController.getM);
 
 router.get("/:id", messageController.getMId);
 
-router.post("/:user", messageController.postUser);
+router.post("/", messageController.postUser);
 
 router.put("/:id", messageController.putId);
 
 router.delete("/:id", messageController.deleteId);
 
-router.get("/?user=username", messageController.getUsername);
+// router.get("/?user=username", messageController.getUsername);
 
 module.exports = router;
